587db349677b959e92a6fa39e8ef90012a09d48f
Lukáš Hejtmánek <hejtmy@gmail.com>
2020-07-20 12:10:40 +0200

    Adds first lesson code

--- code/1-intro-to-R.R
+++ code/1-intro-to-R.R
@@ -1,12 +1,45 @@
-## Example script
+## Task for the coffee break
+
+# Start a new project -
+# Switch between projects - 
+# Change the theme of R studio
+# CHange font size to something you can actually read well
+# Change the margin of code to 80 characters
+
+((4 * 10) %% 3 ) ^ 10
+version
+
+## Packages ------
+
+# Install ggplot
+install.packages("ggplot2")
+# install dplyr
+install.packages("dplyr")
+
+library("ggplot2") # POSSIBLE but not recommended
+library(ggplot2) # GOOD
+library(dplyr)
+
+sessionInfo()
+packageVersion("ggplot2")
+
+## Example script -----
 library(ggplot2)
-head(mpg)
+mpg <- ggplot2::mpg
+head(mpg, n = 1)
 str(mpg)
 summary(mpg)
+
 g <- ggplot(mpg, aes(class))
 g + geom_bar()
-mpg <- ggplot2::mpg
-cty_man_aov <- aov(cty ~ manufacturer, mpg)
-summary(cty_man_aov)
 g <- ggplot(mpg, aes(manufacturer, cty))
 g + geom_boxplot()
+cty_man_aov <- aov(cty ~ manufacturer, mpg)
+summary(cty_man_aov)
+
+## GETTING HELP -----
+
+?head
+help(head)
+
+?rnorm
